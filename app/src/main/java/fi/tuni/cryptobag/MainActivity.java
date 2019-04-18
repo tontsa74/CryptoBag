73d7169239ea3144bebbec04154a87fe2a1b646a
tontsa74 <toni.silventoinen@cs.tamk.fi>
2019-04-18 13:28:59 +0300

    add item delete with long click

--- app/src/main/java/fi/tuni/cryptobag/MainActivity.java
+++ app/src/main/java/fi/tuni/cryptobag/MainActivity.java
@@ -2,11 +2,13 @@ package fi.tuni.cryptobag;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -68,10 +70,18 @@ public class MainActivity extends BaseActivity {
         totalProfit.setText("Total profit: " + total);
 
         final ListView selectedCurrenciesListView = (ListView) findViewById(R.id.selectedCurrenciesListView);
+
         selectedCurrenciesListView.setOnItemClickListener((parent, view, position, id) -> {
             Debug.print(TAG, "setOnItemClickListener",  "position_id: " + position + "_" + id, 2);
             Currency currency = (Currency) selectedCurrenciesListView.getItemAtPosition(position);
             editCurrency(currency, position);
+        });
+
+        selectedCurrenciesListView.setOnItemLongClickListener((parent, view, position, id) -> {
+            Debug.print(TAG, "setOnItemClickListener",  "position_id: " + position + "_" + id, 2);
+            Currency currency = (Currency) selectedCurrenciesListView.getItemAtPosition(position);
+            deleteCurrency(currency, position);
+            return true;
         });
 
         selectedCurrencyArrayAdapter = new ArrayAdapter(this, R.layout.currency_item, R.id.currencyItemTextView, selectedCurrencies) {
@@ -83,6 +93,7 @@ public class MainActivity extends BaseActivity {
                 Currency currency = (Currency) getItem(position);// selectedCurrenciesListView.getItemAtPosition(position);
                 TextView currencyItemTextView = (TextView) rowView.findViewById(R.id.currencyItemTextView);
                 TextView profitItemTextView = (TextView) rowView.findViewById(R.id.profitItemTextView);
+                TextView priceTextView = (TextView) rowView.findViewById(R.id.priceTextView);
                 currencyItemTextView.setText(currency.getName());
                 Double profit = Double.parseDouble(currency.getBag().getProfit());
                 if(profit > 0) {
@@ -91,6 +102,7 @@ public class MainActivity extends BaseActivity {
                     profitItemTextView.setTextColor(Color.RED);
                 }
                 profitItemTextView.setText(currency.getBag().getProfit());
+                priceTextView.setText("" + currency.getPrice());
                 Debug.print(TAG, "selectedCurrencyArrayAdapter",  "position: " + position + currencyItemTextView.getText(), 3);
                 return rowView;//super.getView(position, convertView, parent);
             }
@@ -169,6 +181,33 @@ public class MainActivity extends BaseActivity {
         intent.putExtra("position", -1);
         startActivityForResult(intent, REQUEST_CODE_ADD_BAG);
         apiService.fetch(selectedCurrencies, MEDIUM_PRIORITY);
+    }
+
+    public void deleteCurrency(Currency currency, int position) {
+        Debug.print(TAG, "deleteCurrency()", "edit currency: " + currency + ", pos: " + position, 1);
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(
+                MainActivity.this);
+
+        alert.setTitle("Delete");
+        alert.setMessage("Do you want delete " + currency.getName());
+        alert.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                selectedCurrencies.remove(position);
+                selectedCurrencyArrayAdapter.notifyDataSetChanged();
+
+            }
+        });
+        alert.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        alert.show();
+
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
